64ae2a39dc2ad28bd1e900e24396a9f08a7a109a
Jeff Mac <Jeff.tindell@gmail.com>
2017-01-15 17:51:50 -0500

    init

---
+++ kernel.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Jeff Tindell on 1/15/17.
+//
+
+void kernelMain(void* multiboot_structure, unsigned int magicnumber){
+    printf("Hello World! This is my first operating system.");
+
+    while(1);
+}
